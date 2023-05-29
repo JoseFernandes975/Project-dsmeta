24eba426f54c2cf0baa0181db84dd3173e01e37f
JoseFernandes975 <zehenrique752005@gmail.com>
2023-05-29 14:58:26 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import icon from '../../assets/img/notificationIcon.svg';
 import './styles.css';
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id :number){
+  axios(`${BASE_URL}/sales/${id}/notification`)
+   .then(response => {
+     console.log("Sucesso! Chamada da API notification completa!")
+   })
+}
+
+
+function NotificationButton( {saleId} : Props) {
     return(
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
         <img src={icon} alt="Notificar" />
       </div>
     )
